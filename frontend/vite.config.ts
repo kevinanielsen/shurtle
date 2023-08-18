1a20ced0719f3230e99328b340731bbb8db54c30
Kevin Nielsen <keveran@gmail.com>
2023-08-18 11:01:46 +0200

    build: implement vite-plugin-pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,54 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: "auto",
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      includeAssets: ["/favicon.ico", "/apple-touch-icon.png", "/mask-icon.png"],
+      manifest: {
+        short_name: "Shurtle",
+        name: "Shurtle URL-Shortener",
+        start_url: ".",
+        display: "standalone",
+        background_color: "#fff",
+        theme_color: "#10ABF9",
+        description: "A simple URL-shortener",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
