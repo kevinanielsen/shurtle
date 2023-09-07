272461ae74709dbd2be67a804a00f4ba5efbe035
Kevin Nielsen <keveran@gmail.com>
2023-09-07 12:41:08 +0200

    feat: add link input

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "2c81d9b0ad45b1d70d93e4e5be816fdc"
   }, {
     "url": "index.html",
-    "revision": "0.c0v78oajnc8"
+    "revision": "0.cjt4gn25en8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,52 @@
+import Navbar from "@/components/navbar";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRef } from "react";
+import { IUrl } from "./types/UrlInterface";
+import axios from "axios";
+
+interface ResponseIUrl extends Response {
+  data?: IUrl;
+}
+
+const shortenUrl = async (inputLink: string) => {
+  const shortenedUrl: ResponseIUrl = await axios.post(
+    "https://backend-zglbcovu3q-ew.a.run.app/urls/shorten",
+    {
+      url: inputLink,
+    },
+    {
+      headers: {
+        "content-type": "application/json",
+      },
+    }
+  );
+  console.log(shortenedUrl);
+};
+
 function App() {
-  return <main className="w-screen h-screen flex flex-col items-center"></main>;
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <>
+      <Navbar />
+      <main className="w-screen h-screen flex flex-col items-center justify-center">
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="inputLink">Link to shorten</Label>
+          <Input id="inputLink" type="text" ref={inputRef} />
+          <Button
+            onClick={() => {
+              if (inputRef.current?.value) shortenUrl(inputRef.current?.value);
+            }}
+          >
+            Shorten
+          </Button>
+        </div>
+      </main>
+      ;
+    </>
+  );
 }
 
 export default App;
