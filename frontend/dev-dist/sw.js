faa51ac2750503003ba8562d11a8f095535fe044
Kevin Nielsen <keveran@gmail.com>
2023-09-12 14:57:51 +0200

    feat: add reset button

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "22271febdc61e0ae248cd93e3ec01f59"
   }, {
     "url": "index.html",
-    "revision": "0.859lno76l"
+    "revision": "0.ot9cdroqcq"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,12 @@ import Navbar from "@/components/navbar";
 import { useRef } from "react";
 import { useState } from "react";
 import LoadingModal from "@/components/LoadingModal";
-import ShortenLink from "./components/shorten-link";
+import ShortenLink from "@/components/shorten-link";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const inputRef = useRef<HTMLInputElement>(null);
-  const [shortenedUrl, setShortenedUrl] = useState<string>();
+  const [shortenedUrl, setShortenedUrl] = useState<string | null>();
   const [loading, setLoading] = useState<boolean>(false);
 
   if (loading) return <LoadingModal />
@@ -21,7 +22,12 @@ function App() {
               <p className="text-xl font-bold">
                 Your Result:
               </p>
-              <a href={shortenedUrl} className="text-blue-400 w-fit underline" target="_blank">{shortenedUrl}</a>
+              <a href={shortenedUrl} className="text-blue-400 w-full underline" target="_blank">{shortenedUrl}</a>
+              <Button
+                onClick={() => {
+                  setShortenedUrl(null)
+                }}
+              >New Link</Button>
             </>
           ) : (
             <ShortenLink inputRef={inputRef} setLoading={setLoading} setShortenedUrl={setShortenedUrl} />
