e48d0b0a3d1dbc569412f07e2f884a0afebb1f03
kevinanielsen <keveran@gmail.com>
2023-09-19 17:52:00 +0200

    feat: add analytics component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,13 @@ import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
 import { Toaster } from "@/components/ui/toaster.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
+      <Analytics />
       <Toaster />
     </ThemeProvider>
   </React.StrictMode>
