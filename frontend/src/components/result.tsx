9ac23409673c4d75b3e3db3eec3ae8493ffa490e
kevinanielsen <keveran@gmail.com>
2023-09-19 13:51:18 +0200

    fix: issue with redirect from shortened link

--- frontend/src/components/result.tsx
+++ frontend/src/components/result.tsx
@@ -12,7 +12,7 @@ const Result: React.FC<ResultProps> = ({ shortenedUrl, setShortenedUrl }) => {
       <p className="text-xl font-bold">Your Result:</p>
       <div className="w-full flex px-2 py-1 rounded-lg bg-slate-100 border-slate-950 dark:bg-slate-900 dark:border-slate-100 border-2 items-center justify-between">
         <a
-          href={shortenedUrl}
+          href={`https://${shortenedUrl}`}
           className="text-blue-400 underline text-lg"
           target="_blank"
         >
