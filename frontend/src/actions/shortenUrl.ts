c5ca51048a8c4e341c6c90be5246fef0af294a4a
Kevin Nielsen <keveran@gmail.com>
2023-09-19 10:28:51 +0200

    chore: update error message

--- frontend/src/actions/shortenUrl.ts
+++ frontend/src/actions/shortenUrl.ts
@@ -7,11 +7,12 @@ export interface ResponseIUrl extends Response {
 }
 
 const shortenUrl = async (inputLink: string) => {
-  if (!inputLink.includes("https://")) inputLink = `https://${inputLink}`;
-
   const validUrl = await checkUrl(inputLink);
 
-  if (!validUrl) return Error("Link is not valid, please input a working url!");
+  if (!validUrl)
+    return Error(
+      `Link is not valid, please input a working url! Make sure that the url starts with http:// or https://`
+    );
   const shortenedUrl: ResponseIUrl = await axios.post(
     "https://shurtle.site/urls/shorten",
     {
